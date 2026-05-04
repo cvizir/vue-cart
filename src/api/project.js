47a7ee7972145dd9b6db286d53dbcccf53ee33b1
Sean <cvizir@gmail.com>
2026-05-04 16:57:27 +0800

    修改API的header跟檔名

--- src/api/project.js
+++ src/api/project.js
@@ -3,9 +3,13 @@ import { toFormData } from '@/api/formData'
 
 export function createProject(data) {
   return request({
-    url: '/project_save.php',
+    url: '/fileupload.php',
     method: 'post',
     // 呼叫轉換工具，將複雜物件轉為 FormData
+    headers: {
+      // Optional: Axios generally detects this and sets the boundary automatically
+      'Content-Type': 'multipart/form-data',
+    },
     data: toFormData(data),
     // 重要：上傳大量資料時建議延長 timeout
     timeout: 60000,
